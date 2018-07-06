1c857cf2652899b2027ab559f6905a11080c0369
epstein.w <epstein.w@husky.neu.edu>
2018-07-06 12:22:44 -0400

    registration page's basic jquery pulling

---
+++ src/main/webapp/register.controller.client.js
@@ -0,0 +1,23 @@
+(function() {
+    var registerBtn = jQuery('#registerBtn');
+
+    var usernameFld = $('#username');
+    var passwordFld = $('#password');
+    var password2Fld = $('#password2');
+    registerBtn.click(registerHandler);
+
+
+    function registerHandler() {
+        var usernameStr = usernameFld.val();
+        var passwordStr = passwordFld.val();
+        var password2Str = password2Fld.val();
+
+        //validation of 2 passwords!!!
+        var userObj = {
+            username: usernameStr,
+            password: passwordStr
+        };
+
+        console.log(userObj);
+    }
+})();
