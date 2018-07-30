1695d1b18daaa645cb88b02a1496b485dfc8a1dd
epstein.w <epstein.w@husky.neu.edu>
2018-07-30 02:38:16 -0400

    always forwards

--- src/main/java/com/example/webdevsummer22018serverjava/models/Widget.java
+++ src/main/java/com/example/webdevsummer22018serverjava/models/Widget.java
@@ -11,6 +11,19 @@ public class Widget {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 	private String title;
+	private String widgetType;
+	
+	//heading widget
+	private String text;
+	private int size;
+	
+	//youtube widget
+	private String src;
+	
+	//list widget
+	private String listItems;
+	private boolean ordered;
+	
 	
 	public int getId() {
 		return id;
@@ -23,6 +36,42 @@ public class Widget {
 	}
 	public void setTitle(String title) {
 		this.title = title;
+	}
+	public String getWidgetType() {
+		return widgetType;
+	}
+	public void setWidgetType(String widgetType) {
+		this.widgetType = widgetType;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public int getSize() {
+		return size;
+	}
+	public void setSize(int size) {
+		this.size = size;
+	}
+	public String getSrc() {
+		return src;
+	}
+	public void setSrc(String src) {
+		this.src = src;
+	}
+	public String getListItems() {
+		return listItems;
+	}
+	public void setListItems(String listItems) {
+		this.listItems = listItems;
+	}
+	public boolean isOrdered() {
+		return ordered;
+	}
+	public void setOrdered(boolean ordered) {
+		this.ordered = ordered;
 	}
 
 }
